d2cb693e05a090777e58dfecef26490395bbd0d6
Brittney Coble <bwilcutt#gmail.com>
2022-07-06 02:24:58 -0400

    added test movie code

--- main.go
+++ main.go
@@ -13,11 +13,6 @@ import (
     _ "github.com/lib/pq"
 )
 
-// Go main function
-func main() {
-    fmt.Println("Hello World!")
-}
-
 // db config
 const (
     DB_USER     = "postgres"
@@ -33,4 +28,159 @@ func setupDB() *sql.DB {
     checkErr(err)
 
     return DB
+}
+
+type Movie struct {
+    MovieID   string `json:"movieid"`
+    MovieName string `json:"moviename"`
+}
+
+type JsonResponse struct {
+    Type    string `json:"type"`
+    Data    []Movie `json:"data"`
+    Message string `json:"message"`
+}
+
+// Main function
+func main() {
+
+    // Init the mux router
+    router := mux.NewRouter()
+
+// Route handles & endpoints
+
+    // Get all movies
+    router.HandleFunc("/movies/", GetMovies).Methods("GET")
+
+    // Create a movie
+    router.HandleFunc("/movies/", CreateMovie).Methods("POST")
+
+    // Delete a specific movie by the movieID
+    router.HandleFunc("/movies/{movieid}", DeleteMovie).Methods("DELETE")
+
+    // Delete all movies
+    router.HandleFunc("/movies/", DeleteMovies).Methods("DELETE")
+
+    // serve the app
+    fmt.Println("Server at 8080")
+    log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+// Function for handling messages
+func printMessage(message string) {
+    fmt.Println("")
+    fmt.Println(message)
+    fmt.Println("")
+}
+
+// Function for handling errors
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+    // Get all movies from movies table that don't have movieID = "1"
+    rows, err := db.Query("SELECT * FROM movies")
+
+    // check errors
+    checkErr(err)
+
+    // var response []JsonResponse
+    var movies []Movie
+
+    // Foreach movie
+    for rows.Next() {
+        var id int
+        var movieID string
+        var movieName string
+
+        err = rows.Scan(&id, &movieID, &movieName)
+
+        // check errors
+        checkErr(err)
+
+        movies = append(movies, Movie{MovieID: movieID, MovieName: movieName})
+    }
+
+    var response = JsonResponse{Type: "success", Data: movies}
+
+    json.NewEncoder(w).Encode(response)
+}
+
+// Create a movie
+
+// response and request handlers
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+    movieID := r.FormValue("movieid")
+    movieName := r.FormValue("moviename")
+
+    var response = JsonResponse{}
+
+    if movieID == "" || movieName == "" {
+        response = JsonResponse{Type: "error", Message: "You are missing movieID or movieName parameter."}
+    } else {
+        db := setupDB()
+
+        printMessage("Inserting movie into DB")
+
+        fmt.Println("Inserting new movie with ID: " + movieID + " and name: " + movieName)
+
+        var lastInsertID int
+    err := db.QueryRow("INSERT INTO movies(movieID, movieName) VALUES($1, $2) returning id;", movieID, movieName).Scan(&lastInsertID)
+
+    // check errors
+    checkErr(err)
+
+    response = JsonResponse{Type: "success", Message: "The movie has been inserted successfully!"}
+    }
+
+    json.NewEncoder(w).Encode(response)
+}
+
+// Delete a movie
+
+// response and request handlers
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+
+    movieID := params["movieid"]
+
+    var response = JsonResponse{}
+
+    if movieID == "" {
+        response = JsonResponse{Type: "error", Message: "You are missing movieID parameter."}
+    } else {
+        db := setupDB()
+
+        printMessage("Deleting movie from DB")
+
+        _, err := db.Exec("DELETE FROM movies where movieID = $1", movieID)
+
+        // check errors
+        checkErr(err)
+
+        response = JsonResponse{Type: "success", Message: "The movie has been deleted successfully!"}
+    }
+
+    json.NewEncoder(w).Encode(response)
+}
+
+
+// response and request handlers
+func DeleteMovies(w http.ResponseWriter, r *http.Request) {
+    db := setupDB()
+
+    printMessage("Deleting all movies...")
+
+    _, err := db.Exec("DELETE FROM movies")
+
+    // check errors
+    checkErr(err)
+
+    printMessage("All movies have been deleted successfully!")
+
+    var response = JsonResponse{Type: "success", Message: "All movies have been deleted successfully!"}
+
+    json.NewEncoder(w).Encode(response)
 }
